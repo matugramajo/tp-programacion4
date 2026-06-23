5bff80a34cd6f28e58cc58c5535a6e5613e7bfae
matugramajo <matildegramajo@hotmail.com>
2026-06-23 00:44:55 -0300

    fix: sacar PrecioBase del Bind en ServiciosController

--- Cerrajeria/Controllers/ServiciosController.cs
+++ Cerrajeria/Controllers/ServiciosController.cs
@@ -56,7 +56,7 @@ namespace Cerrajeria.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,PrecioBase,Activo")] Servicio servicio)
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,Activo")] Servicio servicio)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace Cerrajeria.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,PrecioBase,Activo")] Servicio servicio)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,Activo")] Servicio servicio)
         {
             if (id != servicio.Id)
             {
